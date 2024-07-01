06533701dbd026b6b326b1d219fbfcd89fe16040
JEREMY <christabelajoku@Christabels-MacBook-Air.local>
2024-06-30 23:07:59 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './Header.jsx'
+import Meme from './Meme.jsx'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Meme />
+    </div>
+  )
+  
+}
+
+export default App
